ceb13a972dcbe263aed362be4012d27cbc76cace
Andrew Li <207803156+andrewrli@users.noreply.github.com>
2025-10-31 13:33:14 -0700

    failing edit_distance() test case

--- tests/test_edit_distance.py
+++ tests/test_edit_distance.py
@@ -5,3 +5,8 @@ def test_edit_distance_example():
     word1 = "kitten"
     word2 = "sitting"
     assert edit_distance(word1, word2) == 3
+
+def test_edit_distance_insert_at_start():
+    word1 = "abc"
+    word2 = "dabc"
+    assert edit_distance(word1, word2) == 1
