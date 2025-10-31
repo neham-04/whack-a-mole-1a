09fd0ae4c7d83ab3d43c93b3452db12734a3feea
RJ-06 <rahul@Rahul-Laptop.>
2025-10-31 13:40:18 -0700

    added failing test case

--- tests/test_coin_change.py
+++ tests/test_coin_change.py
@@ -5,3 +5,13 @@ def test_coin_change_example():
     coins = [1, 2, 5]
     amount = 11
     assert coin_change(coins, amount) == 3
+ 
+def test_coin_change_no_solution():
+    coins = [2]
+    amount = 3
+    assert coin_change(coins, amount) == -1
+
+def test_coin_change_exact_solution():
+    coins = [1, 3, 4]
+    amount = 6
+    assert coin_change(coins, amount) == 2
