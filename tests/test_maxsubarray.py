4da21f944b6d76bb8942efce6fa347e75522739c
Parnika <parnika.chaturvedi@gmail.com>
2025-10-31 13:23:50 -0700

    failing max subarray test case

--- tests/test_maxsubarray.py
+++ tests/test_maxsubarray.py
@@ -4,3 +4,7 @@ from algo import max_subarray
 def test_max_subarray_example():
     nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
     assert max_subarray(nums) == 6
+
+def test_max_subarray_all_negative():
+    nums = [-5, -2, -3]
+    assert max_subarray(nums) == -2
